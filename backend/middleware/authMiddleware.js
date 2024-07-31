a11e5a5b60264f7c95a742c9f0ccac736fbd0df5
corey sloan <sloancb@mail.uc.edu>
2024-07-31 19:57:55 -0400

    Deprecated middleware because it does not work

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ message: 'Access Denied. No token provided.' });
+    //TODO deprecated because siginng and handling does not work
+    // const token = req.header('Authorization');
+    // if (!token) return res.status(401).json({ message: 'Access Denied. No token provided.' });
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (ex) {
-        res.status(400).json({ message: 'Invalid token.' });
-    }
+    // try {
+    //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //     req.user = decoded;
+    // } catch (ex) {
+    //     res.status(400).json({ message: 'Invalid token.' });
+    // }
+    next();
 };
